e0bff0c3c0239429eed7f9d5fe82a71504ab911f
Jonathan Lipps <jlipps@gmail.com>
2013-10-24 17:08:21 -0700

    getting some objects hooked up

--- focus.js
+++ focus.js
@@ -1,13 +1,31 @@
+/*global Meteor:true, Accounts:true, Template:true */
+"use strict";
+
+var Teams = new Meteor.Collection("teams");
+var Goals = new Meteor.Collection("goals");
+var Tasks = new Meteor.Collection("tasks");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to focus.";
+  Accounts.ui.config({
+    requestPermissions: {
+      github: ['user']
+    },
+    passwordSignupFields: 'USERNAME_AND_EMAIL'
+  });
+
+  Meteor.subscribe("teams");
+
+  Template.yourTeams.teams = function() {
+    return Teams
+      .find({$or: [{owner: Meteor.userId}, {member: Meteor.userId}]})
+      .fetch();
   };
 
-  Template.hello.events({
+  Template.yourTeams.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        console.log(this);
     }
   });
 }
@@ -15,5 +33,11 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  Meteor.publish("teams", function() {
+    return Teams.find({$or: [{invited: this.userId},
+                             {owner: this.userId},
+                             {member: this.userId}]});
   });
 }
